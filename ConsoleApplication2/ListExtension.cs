1dc3744a4397456539e1a8a2b74dc7f1badea0f9
Shanmuga <shanmuga.sundaram@turner.com>
2015-07-29 10:06:23 -0400

    List Difference logic added

---
+++ ConsoleApplication2/ListExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public static class ListExtension
+    {
+        public static List<string> Difference(this List<string> list1, List<string> list2)
+        {
+
+            var allListContent = list1.Union(list2);
+
+            var difference1 = allListContent.Except(list2);
+
+            var difference2 = allListContent.Except(list1);
+
+            return difference1.Union(difference2).ToList();
+        }
+    }
+}
--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,6 +10,32 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+
+            var list1 = new List<string> { "A", "B", "C", "D", "X" };
+
+            var list2 = new List<string> { "A", "B" };
+
+            var firstTest = list1.Difference(list2);
+
+            PrintResults(firstTest);
+
+            var list3 = new List<string> {"X", "Y", "Z", "A"};
+
+            var secondResult = list3.Difference(list1);
+
+            PrintResults(secondResult);
+
+            Console.ReadKey();
+        }
+
+        static void PrintResults(List<string> results)
+        {
+            Console.WriteLine("Result:");
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+
         }
     }
 }
